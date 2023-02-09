2adca37ce987b2af84b8027fb2097e7de2ef16c6
amalchaoui <amal.ch.mp@gmail.com>
2023-02-09 12:36:14 +0100

    modified notebooks

--- lib/text_pre_processing.py
+++ lib/text_pre_processing.py
@@ -2,11 +2,8 @@
 import re
 from bs4 import BeautifulSoup
 
-
-
-
-# Function to remove tags
 def remove_tags(corpus):
+    """ Remove html tags"""
     cln_corpus = []
     for sentence in corpus:
         # parse html content
@@ -19,15 +16,42 @@ def remove_tags(corpus):
     # return clean corpus
     return cln_corpus
 
-     
-            
-def remove_anomalies(corpus):
+
+def remove_q_heading(text_str):
+    """ Detects positions of 'mr' and /or 'mme' in text and returns stripped text """
+    text_lst = text_str.split()
+    list_mr_mme=["mr", "mme", "demr", "mmme", "demme", "mmr", "interrogemme"]
+    if text_lst[0][0]=='"':
+        text_lst[0] = text_lst[0][1:]
+    i = 0
+    while i<len(list_mr_mme):
+        try:
+            idx1 = text_lst.index(list_mr_mme[i])
+            if idx1 > 20: i+=1
+            else: break
+        except:
+            i+=1
+
+    i = 0
+    while i<len(list_mr_mme):
+        try:
+            idx2 = (text_lst[:idx1] + ['xx'] + text_lst[idx1+1:] ).index(list_mr_mme[i])
+            if idx2 > 20: i+=1
+            else: break
+        except:
+            i+=1
+
+    (idx1, idx2) = (idx1, idx2) if idx1 < idx2 else (idx2, idx1)
+    return ' '.join(text_lst[:idx1] + text_lst[idx2+3:])
+
+   
+def remove_anomalies(corpus, rmv_heading=True):
     """ replace apostrophy inconsistencies \ ' with real apostrophies. ' 
         remove \xa0 signs.
         remove urls.
     """
     cln_corpus = []
-    for sentence in corpus:
+    for i, sentence in enumerate(corpus):
         sentence = sentence.replace("M.", "Mr")
         sentence = sentence.replace(u'\xa0', u' ')
         sentence = sentence.replace("\'", "' ")
@@ -38,5 +62,5 @@ def remove_anomalies(corpus):
         # put space between words and punctuation 
         sentence = re.sub('([.,!?()])', r' \1 ', sentence)
         sentence = re.sub('\s{2,}', ' ', sentence)
-        cln_corpus.append(sentence)
+        cln_corpus.append(remove_q_heading(sentence))
     return cln_corpus
